cb37ff67535eff9b4e258c675f694b41c77b3954
Viet Phung <fuvavi.com@gmail.com>
2025-12-30 14:46:39 +0700

    Chapter 11 Mutating Data

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,6 +8,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', '1');
@@ -18,6 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
